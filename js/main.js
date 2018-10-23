ce5c6d0bf38ffc8c7d7a91d090eb0986141f1ba9
skndungu <skndungu75@outlook.com>
2018-10-23 11:49:03 -0700

    created several routes, pledge and beneficiaries

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+const SubmitBtn = document.querySelector('#btn-submit');
+
+
+let showMenu = false;
+let submit = false;
+
+if(menuBtn) {
+    menuBtn.addEventListener('click', toggleMenu);
+  }
+  
+
+  function toggleMenu() {
+    if (!showMenu) {
+      menuBtn.classList.add('close');
+      menu.classList.add('show');
+      menuNav.classList.add('show');
+      menuBranding.classList.add('show');
+      navItems.forEach(item => item.classList.add('show'));
+  
+      // Set Menu State
+      showMenu = true;
+    } else {
+      menuBtn.classList.remove('close');
+      menu.classList.remove('show');
+      menuNav.classList.remove('show');
+      menuBranding.classList.remove('show');
+      navItems.forEach(item => item.classList.remove('show'));
+  
+      // Set Menu State
+      showMenu = false;
+    }
+  }
+
+
+  if(SubmitBtn) {
+    SubmitBtn.addEventListener('click',formsubmission);
+  }
+  
+  function formsubmission (){
+    if (!submit){
+      alert("Your details were succesfully sent Thanks");
+    }
+  }
+  
+  
