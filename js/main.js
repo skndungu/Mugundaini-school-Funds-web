c4bb0d47f00cbe8f02132f313858bfff366bc6b4
skndungu <skndungu75@outlook.com>
2018-10-24 00:09:51 -0700

    style changes

--- js/main.js
+++ js/main.js
@@ -49,4 +49,43 @@ if(menuBtn) {
     }
   }
   
+
+  // Get modal element
+
+var modal = document.getElementById('simpleModal');
+
+//Get open Modal Button
+var modalBtn = document.getElementById('modalBtn');
+
+//Close Button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+//Listen for open click
+modalBtn.addEventListener('click', openModal);
+//Listen for close click
+closeBtn.addEventListener('click', closeModal);
+//Listen for outside click
+window.addEventListener('click', clickOutside);
+
+//Function to open Modal 
+function openModal() {
+    // console.log(142);
+    modal.style.display = 'block';
+}
+
+//Function to close Modal 
+function closeModal() {
+    // console.log(142);
+    modal.style.display = 'none';
+}
+
+//Function to close Modal if outside click 
+function clickOutside(e) {
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+   
+}
+
+
   
